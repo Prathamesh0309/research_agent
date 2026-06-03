9d9f1e279884280d1db76198eb0f77b433489be0
Prathamesh Deshpande <prathamesh@dhcp-10-134-231-193.uawifi.arizona.edu>
2026-06-03 15:02:14 -0700

    adding evals

--- evals/test_planner.py
+++ evals/test_planner.py
@@ -1,8 +1,8 @@
 from schemas import ResearchState
-from planner import run_planner
-from searcher import run_searcher
-from critic import run_critic
-from writer import run_writer
+from agents.planner import run_planner
+from agents.searcher import run_searcher
+from agents.critic import run_critic
+from agents.writer import run_writer
 
 state = ResearchState(query="How does retrieval augmented generation work?")
 state = run_planner(state)
